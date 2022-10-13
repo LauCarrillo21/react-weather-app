7a64da1a728c3c46180d1baca4c656c1fbd283aa
LauCarrillo <laucarrillo21@gmail.com>
2022-10-12 19:46:53 -0500

    Added github repo  url

--- src/App.js
+++ src/App.js
@@ -4,7 +4,16 @@ import './App.css';
 function App() {
   return (
    <div className='App'>
+    <h1>
     Weather App
+    </h1>
+      <footer>
+        This project was coded by
+        <a href="https://www.linkedin.com/feed/" target="_blank" rel="noreferrer"> Lau Carrillo
+        </a> and is
+        <a href="https://github.com/LauCarrillo21/react-weather-app" target="_blank" rel="noreferrer"> open-sourced on Github
+        </a>
+      </footer>
    </div>
   );
 }
