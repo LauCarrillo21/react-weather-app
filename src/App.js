0c6e7577072518cf01996bbe4eb40df2b974d3d4
LauCarrillo <laucarrillo21@gmail.com>
2022-10-13 20:05:54 -0500

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 function App() {
   return (
    <div className='App'>
+    <div className="container">
     <h1>
     Weather App
     </h1>
@@ -14,6 +15,7 @@ function App() {
         <a href="https://github.com/LauCarrillo21/react-weather-app" target="_blank" rel="noreferrer"> open-sourced on Github
         </a>
       </footer>
+      </div>
    </div>
   );
 }
